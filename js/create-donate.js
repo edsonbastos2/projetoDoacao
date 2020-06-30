be991e8d157b071b3666dbbc34d176351e63991f
Edson Bastos <edsonbastos2@hotmail.com>
2020-06-29 22:18:44 -0300

    Feito lógica para adicionar itens no formulário

--- js/create-donate.js
+++ js/create-donate.js
@@ -22,6 +22,9 @@ function getCities(event) {
   const indexOfSelectedState = event.target.selectedIndex;
   stateInput.value = event.target.options[indexOfSelectedState].text
   const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${city}/municipios`
+
+  cities.innerHTML = "<option value>Selecione a Cidade</option>";
+  cities.disabled = true;
   fetch(url)
     .then(resp => resp.json())
     .then(data => {
@@ -34,7 +37,42 @@ function getCities(event) {
 
 }
 
-document.querySelector('[data-states="uf"]').addEventListener('change', getCities)
+document.querySelector('[data-states="uf"]').addEventListener('change', getCities);
 
+// Pegar todos os Itens
+
+const itemsToCollect = document.querySelectorAll('.item-grid li')
+const collectedItem = document.querySelector('input[name=items]')
+
+// Função para adicionar e remover items 
+let selectedItem = [];
+function handleSelectedItem(event) {
+  const itemLi = event.target;
+  itemLi.classList.toggle('selected')
+
+  const itemId = itemLi.dataset.id;
+
+  // Verifica se existem itens selecionados, se sim
+  // Pega os itens selecionados
+  const alreadySelected = selectedItem.findIndex(item => item == itemId);
+
+
+  // Se já estiver selecionado
+  if (alreadySelected != -1) {
+    // Tira da seleção
+    const filterdItems = selectedItem.filter(item => item != itemId)
+
+    selectedItem = filterdItems
+  } else {
+    // se não estiver selecionado adicionar a seleção
+    selectedItem.push(itemId)
+  }
+
+  collectedItem.value = selectedItem
+}
+
+itemsToCollect.forEach(item => {
+  item.addEventListener('click', handleSelectedItem)
+})
 
 
